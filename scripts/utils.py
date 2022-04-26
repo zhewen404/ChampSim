b10e49004d42df960877de3b0727842515c7074c
Zhewen <zhewen.pan@wisc.edu>
2022-04-26 14:44:43 -0500

    updating rollup scripts

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,16 +3,22 @@ from scipy.stats import gmean
 reduction_map = {
     'ipc': lambda list_ : gmean(list_),
     'l1d_mr': lambda list_: sum(list_) / len(list_),
+    'l2_mr': lambda list_: sum(list_) / len(list_),
+    'llc_mr': lambda list_: sum(list_) / len(list_),
 }
 
 prune_map = {
+    'num_core': lambda line: int(line.split('Number of CPUs: ')[-1]),
     'ipc': lambda line: float(line.split(' instructions')[0].split('IPC: ')[-1]),
     'l1d_mr': lambda line: float(line.split('MISS:      ')[-1])/float(line.split('  HIT')[0].split('ACCESS:     ')[-1])*100,
-
+    'l2_mr': lambda line: float(line.split('MISS:      ')[-1])/float(line.split('  HIT')[0].split('ACCESS:     ')[-1])*100,
+    'llc_mr': lambda line: float(line.split('MISS:      ')[-1])/float(line.split('  HIT')[0].split('ACCESS:     ')[-1])*100,
 }
 
 pattern_map = {
+    'num_core': 'Number of CPUs: \d+',
     'ipc': "CPU \d{1,2} cumulative IPC: ",
     'l1d_mr': 'cpu\d{1,2}_L1D TOTAL\s+ACCESS:\s+\d+\s+HIT:\s+\d+\s+MISS:\s+\d+',
-
+    'l2_mr': 'cpu\d{1,2}_L2C TOTAL\s+ACCESS:\s+\d+\s+HIT:\s+\d+\s+MISS:\s+\d+',
+    'llc_mr': 'LLC TOTAL\s+ACCESS:\s+\d+\s+HIT:\s+\d+\s+MISS:\s+\d+',
 }
--- scripts/write_stats.py
+++ scripts/write_stats.py
@@ -13,7 +13,7 @@ def construct_argparser():
                         )
     return parser
 
-def get_list(file, metric):
+def get_list(file, metric, override_ri=False):
     pattern = pattern_map[metric]
     ri_pattern = 'Region of Interest Statistics'
 
@@ -24,7 +24,7 @@ def get_list(file, metric):
             if re.match(ri_pattern, line):
                 region_interest_hit = True
             if re.match(pattern, line):
-                if region_interest_hit:
+                if region_interest_hit or override_ri:
                     prune_func = prune_map[metric]
                     res = prune_func(line)
                     ipc_list.append(res)
@@ -34,17 +34,35 @@ if __name__ == "__main__":
     parser = construct_argparser()
     args = parser.parse_args()
 
+    num_core = get_list(args.file, 'num_core', True)[0]
+    print(f'*** num core={num_core}')
+
     metric = 'ipc'
     ipc_list = get_list(args.file, metric)
+    assert len(ipc_list) == num_core, 'missing ipc data'
     reduction_func = reduction_map[metric]
     ipc_gm = reduction_func(ipc_list)
-    print(f'ipc_gm={ipc_gm}')
+    print(f'ipc_gm={ipc_gm:.3f}')
 
     metric = 'l1d_mr'
     l1d_mr_list = get_list(args.file, metric)
-    print(l1d_mr_list)
+    assert len(l1d_mr_list) == num_core, 'missing l1d mr data'
     reduction_func = reduction_map[metric]
     l1d_mr_gm = reduction_func(l1d_mr_list)
-    print(l1d_mr_gm)
+    print(f'l1d_mr_gm={l1d_mr_gm:.2f}%')
+
+    metric = 'l2_mr'
+    l2_mr_list = get_list(args.file, metric)
+    assert len(l2_mr_list) == num_core, 'missing l2 mr data'
+    reduction_func = reduction_map[metric]
+    l2_mr_gm = reduction_func(l2_mr_list)
+    print(f'l2_mr_gm={l2_mr_gm:.2f}%')
+
+    metric = 'llc_mr'
+    llc_mr_list = get_list(args.file, metric)
+    assert len(llc_mr_list) == num_core, 'missing llc mr data'
+    reduction_func = reduction_map[metric]
+    llc_mr_gm = reduction_func(llc_mr_list)
+    print(f'llc_mr_gm={llc_mr_gm:.2f}%')
 
     print('done')
